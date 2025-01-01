754429805b8e6d4880018a521af979faf6c6925e
dvdr1029 <david.tom.rau@gmail.com>
2025-01-01 14:07:20 -0500

    creating generic starter files for each day

--- code_files/day_starter.py
+++ code_files/day_starter.py
@@ -4,7 +4,15 @@ from session_cookie import session_cookie
 
 class DayStarter:
 
-    def __new__(self, day):
+    def __new__(self, day, micro: bool = False):
+        if micro:
+            with open(f'input_files/day{day}input_micro.txt', 'r') as f:
+                return f.read()
+        try:
+            with open(f'input_files/day{day}input.txt', 'r') as f:
+                return f.read()
+        except FileNotFoundError:
+            pass
         self.day = day
         self.url = f"https://adventofcode.com/2024/day/{day}/input"
         self.cookies = {
@@ -13,7 +21,7 @@ class DayStarter:
         self.day_input = requests.get(self.url, cookies=self.cookies)
         if self.day_input.status_code == 200:
             self.input = self.day_input.text
-            with open(f'input_files/day{day}input_testing.txt', 'w') as f:
+            with open(f'input_files/day{day}input.txt', 'w') as f:
                 f.write(self.input)
             return self.input
         else:
@@ -21,5 +29,5 @@ class DayStarter:
 
 
 if __name__ == '__main__':
-    mgr = DayStarter(1)
-    print(mgr)
+    for day in range(26):
+        mgr = DayStarter(day)
